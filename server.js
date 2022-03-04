bf169099ebdea4d6ddfe5b702c1c1ba421d517b5
NicolasRojas-CENG <55503463+NicolasRojas-CENG@users.noreply.github.com>
2022-03-04 01:31:52 -0500

    Working on saving tasks.

--- server.js
+++ server.js
@@ -1,45 +1,34 @@
 //Importing Express.js
 const express = require("express");
+//Importing the database.
+const db = require("./db/db.json");
 //Importhing path module.
 const path = require('path');
-// const index = require("./public/index.html");
+
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
-
 const app = express();
-
-// var options = {
-//     dotfiles: 'ignore',
-//     etag: false,
-//     extensions: ['htm', 'html'],
-//     index: false,
-//     maxAge: '1d',
-//     redirect: false,
-//     setHeaders: function (res, path, stat) {
-//       res.set('x-timestamp', Date.now())
-//     }
-//   }
 
 app.use(express.static('public'));
 
-//Route for the landing page.
-// app.get("/", (req, res) => {
-//     res.sendFile(path.join(__dirname, '/index.html'));
-// });
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json())
 
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, '/notes.html'));
-});
+app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, '/notes.html')));
 
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-});
+app.route('/api/notes')
+.get((req, res) => res.json(db))
+.post((req, res) =>  writeToFile(req.body));
 
-/*Note:
-For the route paths, we can use regular expressions.
-- "/abc" => /abc normal
-- "/ab?cd" => /acd or /abcd (Zero or more of the character before "?")
-- "/ab+cd" => /abcd, /abbcd, abbbcd, etc. (One or more of the character before +)
-- "/ab*cd" => (/ab + anything + cd) (* = any character, any number of said character)
-- "/*man$/" => /ndhaman, /treman, /xerman, etc. (Anything ending with "man")
-*/
+const writeToFile = body => {
+    console.log(body);
+    db.push(body);
+    var entries = JSON.stringify(db);
+    fs.writeFile("./db/db.json", entries, function (err) {
+        if (err) throw err;
+        console.log('Task was successfully saved!');
+    });
+}
+
+app.listen(PORT, () => console.log(`API server now on port ${PORT}!`));

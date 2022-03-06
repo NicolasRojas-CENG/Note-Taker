f556f52c67c7bd8744f60dd675427c82f718d8ce
NicolasRojas-CENG <55503463+NicolasRojas-CENG@users.noreply.github.com>
2022-03-05 20:51:12 -0500

    Added some documentation, and moved around some code.

--- server.js
+++ server.js
@@ -2,58 +2,45 @@
 const express = require("express");
 //Importing the database.
 const db = require("./db/db.json");
-//Importhing path module.
+//Importing path module.
 const path = require('path');
-
+//Importing file system module.
 const fs = require('fs');
-const { json } = require("body-parser");
-
+//Setting the port to be used.
 const PORT = process.env.PORT || 3000;
+
 const app = express();
 
 app.use(express.static('public'));
-
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, '/notes.html')));
 
 app.route('/api/notes')
-.get((req, res) => readFromFile(false, null, res))
-.post((req, res) =>  readFromFile(true, req.body, res));
+.get((req, res) => showTasks(res))
+.post((req, res) =>  saveTask(req.body, res));
 
 app.delete('/api/notes/:id', (req, res) => removeTask(req.params.id, res));
 
 app.get('/*/', (req, res) => res.redirect('/index.html'))
 
-const readFromFile = (write, body, res) => {
-    let data = fs.readFileSync('./db/db.json', 'utf-8');
-    data = JSON.parse(data);
-    (write ? writeToFile(data, body, res): res.send(data));
+//Function used to show the saved tasks.
+const showTasks = (res) => {
+   const data = readFromFile();
+    res.send(data);
 }
 
-const writeToFile = (data, body, res) => {
-    data.push(body);
-    addId(data);
-    data = JSON.stringify(data);
-    console.log(data);
-    fs.writeFile("./db/db.json", data, function (err) {
-        (err ? res.send('Something went wrong!') : res.send('Task was successfully saved!'));
-    });
+//Function used to save a task.
+const saveTask = (body, res) => {
+   const data = readFromFile();
+   data.push(body);
+   writeToFile(data, res);
 }
 
-const addId = (data) => {
-    var id = 1;
-    console.log(id);
-    data.forEach(element => {
-        element["id"] = id++;
-    });
-    return data;
-}
-
+//Function used to remove a task.
 const removeTask = (id, res) => {
-    let data = fs.readFileSync('./db/db.json', 'utf-8');
-    data = JSON.parse(data);
+    const data = readFromFile();
     data = data.filter(function callback(item){
         if(item.id != id){
             return true
@@ -61,12 +48,34 @@ const removeTask = (id, res) => {
             return false
         }
     });
-    addId(data);
-    console.log(data);
+    writeToFile(body, res);
+}
+
+//Function used to get the saved tasks from the "database".
+const readFromFile = () => {
+    let data = fs.readFileSync('./db/db.json', 'utf-8');
+    return data = JSON.parse(data);
+    //(write ? writeToFile(data, body, res): res.send(data));
+}
+
+//Function to save the list of tasks to the "database".
+const writeToFile = (data, res) => {
+    //data.push(body);
+    data = addId(data);
     data = JSON.stringify(data);
     fs.writeFile("./db/db.json", data, function (err) {
         (err ? res.send('Something went wrong!') : res.send('Task was successfully saved!'));
     });
+}
+
+//Function used to add IDs to the tasks.
+const addId = (data) => {
+    var id = 1;
+    //console.log(data);
+    data.forEach(element => {
+        element["id"] = id++;
+    });
+    return data;
 }
 
 app.listen(PORT, () => console.log(`API server now on port ${PORT}!`));

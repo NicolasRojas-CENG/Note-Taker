e9066458a53562d43abbd1a6b239bc6c6e49dd5a
NicolasRojas-CENG <55503463+NicolasRojas-CENG@users.noreply.github.com>
2022-03-05 19:37:14 -0500

    Finished the logic for the deletion of a task.

--- server.js
+++ server.js
@@ -19,18 +19,53 @@ app.use(express.json())
 app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, '/notes.html')));
 
 app.route('/api/notes')
-.get((req, res) => res.json(db))
-.post((req, res) =>  writeToFile(req.body, res));
+.get((req, res) => readFromFile(false, null, res))
+.post((req, res) =>  readFromFile(true, req.body, res));
 
-const writeToFile = (body, res) => {
+app.delete('/api/notes/:id', (req, res) => removeTask(req.params.id, res));
+
+app.get('/*/', (req, res) => res.redirect('/index.html'))
+
+const readFromFile = (write, body, res) => {
     let data = fs.readFileSync('./db/db.json', 'utf-8');
     data = JSON.parse(data);
+    (write ? writeToFile(data, body, res): res.send(data));
+}
+
+const writeToFile = (data, body, res) => {
     data.push(body);
+    addId(data);
     data = JSON.stringify(data);
     console.log(data);
     fs.writeFile("./db/db.json", data, function (err) {
-        if (err) throw err;
-        res.send('Task was successfully saved!');
+        (err ? res.send('Something went wrong!') : res.send('Task was successfully saved!'));
+    });
+}
+
+const addId = (data) => {
+    var id = 1;
+    console.log(id);
+    data.forEach(element => {
+        element["id"] = id++;
+    });
+    return data;
+}
+
+const removeTask = (id, res) => {
+    let data = fs.readFileSync('./db/db.json', 'utf-8');
+    data = JSON.parse(data);
+    data = data.filter(function callback(item){
+        if(item.id != id){
+            return true
+        } else {
+            return false
+        }
+    });
+    addId(data);
+    console.log(data);
+    data = JSON.stringify(data);
+    fs.writeFile("./db/db.json", data, function (err) {
+        (err ? res.send('Something went wrong!') : res.send('Task was successfully saved!'));
     });
 }
 
